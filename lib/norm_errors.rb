50f072e4fd1a79e783d6adb8a4076f62868d3861
Burak Ersoy <burakersoy@msn.com>
2022-12-04 02:02:55 +0300

    add error module and add classes for bad request, conflict

---
+++ lib/norm_errors.rb
@@ -0,0 +1,39 @@
+module NormErrors
+  class BadRequest < StandardError
+    def initialize(message = nil)
+      super(message)
+    end
+  end
+
+  class Conflict < StandardError
+    def initialize(message = nil)
+      super(message)
+    end
+  end
+end
+
+module NormSerializers
+  class UnprocessableEntity
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+
+    def as_json
+      { errors: errors }
+    end
+
+    private
+
+    def errors
+      object.errors.attribute_names.map do |error_key|
+        {
+          source: ActiveModelSerializers::Adapter::JsonApi::Error.error_source(:pointer, error_key),
+          title:  object.errors.messages[error_key].first,
+          detail: object.errors.full_messages_for(error_key).first
+        }
+      end
+    end
+  end
+end
