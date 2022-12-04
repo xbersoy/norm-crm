eb958ce042a119f2c11f134ef7559c19c17ceae5
Burak Ersoy <burakersoy@msn.com>
2022-12-04 03:33:00 +0300

    remove unused controllers

--- app/controllers/documentation_controller.rb
+++
@@ -1,8 +0,0 @@
-class DocumentationController < ApplicationController
-  skip_before_action :authenticate_user!
-  layout 'example'
-
-  def index
-    @user = User.new
-  end
-end
--- app/controllers/examples_controller.rb
+++
@@ -1,7 +0,0 @@
-class ExamplesController < ApplicationController
-  skip_before_action :authenticate_user!
-  layout 'example'
-
-  def index
-  end
-end
