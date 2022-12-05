80d9a6322b632b930816d4044eadb7b7f89fd12b
Burak Ersoy <burakersoy@msn.com>
2022-12-06 01:44:33 +0300

    comment out devise js

--- app/javascript/devise.js
+++ app/javascript/devise.js
@@ -1,10 +1,8 @@
-
 (function () {
-    'use strict'
-    setInterval(myTimer, 1000);
-debugger;
-    function myTimer() {
-
-    console.log('testttstststweesststsetstste');
-    }
+  "use strict";
+  // setInterval(myTimer, 1000);
+  // debugger;
+  function myTimer() {
+    console.log(document.getElementById("user_email"));
+  }
 })();
