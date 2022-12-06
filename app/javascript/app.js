2e4430ebfc87acacccc2107de7cf23b56bbc7b1f
Burak Ersoy <burakersoy@msn.com>
2022-12-06 03:37:50 +0300

    delete innecessary line

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
-import "controllers/fill_fields"
 import * as bootstrap from "bootstrap"
 
 // required opt-in - initialize tooltips
