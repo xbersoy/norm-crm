3a7cc5aa3e20d164b38395e92a6b8540c42440c1
Burak Ersoy <burakersoy@msn.com>
2022-12-06 03:37:16 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Rails.logger.info 'Creating User....'
+
+User.create(
+  first_name: 'Burak',
+  last_name: 'Ersoy',
+  password: '12345678',
+  password_confirmation: '12345678',
+  email: 'burakersoy@msn.com'
+)
