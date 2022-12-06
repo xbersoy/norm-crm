ce62987560d6ddedeb6cf8c74ae72117bd9fcb48
Burak Ersoy <burakersoy@msn.com>
2022-12-07 02:11:50 +0300

    overwrite default registrations controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
   resource :two_factor_settings, except: %i[index show]
 
   root to: 'home#index'
