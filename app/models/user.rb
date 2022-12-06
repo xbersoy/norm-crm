422c40db581829c4911a99cbee2d9f322ffb8ef2
Burak Ersoy <burakersoy@msn.com>
2022-12-06 03:40:31 +0300

    comment out custom validatable import

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,7 @@
 require 'devise/models/custom_validatable'
 
 class User < ApplicationRecord
-  include Devise::Models::CustomValidatable
-  serialize :fruit, JSON
-  serialize :music, JSON
-  serialize :choises, JSON
-
-  AVATAR = Mime::LOOKUP.keys.keep_if{ |v| v =~ /image/ }
-  COLOR = [:red, :pink, :violet, :indigo, :blue, :teal, :green, :yellow]
-  FRUIT = [:apple, :banana, :cherry, :coconut, :grape, :lime, :mango, :orange, :pear, :pineapple]
-  MUSIC = [:rock, :pop, :jazz, :heavy_metal, :hip_hop, :house, :electronic_dance, :dance, :techno]
-  LANGUAGE = [:en, :de, :es, :ru]
-  PILL = [:red, :blue]
-  CHOISES = [:a, :b, :c, :d]
+  # include Devise::Models::CustomValidatable
 
   devise :two_factor_authenticatable, :two_factor_backupable,
          otp_backup_code_length: 10, otp_number_of_backup_codes: 10,
