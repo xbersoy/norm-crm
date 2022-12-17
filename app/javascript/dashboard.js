cfcd51d8d6e9c7adc21c10cfc465e18951c92f15
Burak Ersoy <burakersoy@msn.com>
2022-12-18 01:24:04 +0300

    backup

--- app/javascript/dashboard.js
+++ app/javascript/dashboard.js
@@ -6,20 +6,8 @@ eagerLoadControllersFrom("dashboard", dashboard)
 
 const body = document.querySelector("body"),
   sidebar = body.querySelector("nav"),
-  toggle = body.querySelector(".toggle"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
+  toggle = body.querySelector(".toggle")
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
-});
-
-modeSwitch.addEventListener("click", () => {
-  body.classList.toggle("dark");
-
-  if (body.classList.contains("dark")) {
-    modeText.innerText = "Light mode";
-  } else {
-    modeText.innerText = "Dark mode";
-  }
 });
