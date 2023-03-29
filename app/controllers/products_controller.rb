5a3356259efbaf4dc341f691f2784aa8a2a5a35a
Burak Ersoy <burakersoy@msn.com>
2023-03-29 12:29:29 +0300

    add products controller and view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+  layout 'dashboard'
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get 'dashboard', to: 'dashboard#index'
+  get 'products', to: 'products#index'
 end
