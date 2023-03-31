c16e65c9438204058d94b2033d1a5a358477aa7c
Burak Ersoy <burakersoy@msn.com>
2023-03-31 23:23:12 +0300

    change import oder in importmap config

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,10 +10,10 @@ pin 'devise'
 pin 'dashboard'
 pin '@hotwired/stimulus', to: 'stimulus.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js'
 pin 'bootstrap/icons', to: 'https://ga.jspm.io/npm:bootstrap-icons@1.9.1/font/bootstrap-icons.css'
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/esm/index.js'
 pin 'stimulus', to: 'stimulus.js', preload: false # proxy for @hotwired/stimulus
 pin 'stimulus-password-visibility', to: 'https://ga.jspm.io/npm:stimulus-password-visibility@1.1.1/dist/stimulus-password-visibility.es.js'
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
