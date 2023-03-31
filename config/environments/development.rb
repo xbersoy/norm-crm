bcd222f9a01cf622857b92748a75b510652c5f0c
Burak Ersoy <burakersoy@msn.com>
2023-03-31 04:09:15 +0300

    add example host to whitelist

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << NORM_APP_HOST # Whitelist one hostname
   config.hosts << /uygulama\.norm\.localhost\Z/ # Whitelist a test domain
+  config.hosts << "www.example.com"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
