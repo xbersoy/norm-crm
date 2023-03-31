a6026935f98d7fb973f10af3d4c8926e0436684c
Burak Ersoy <burakersoy@msn.com>
2023-03-31 04:12:06 +0300

    update dashboard layout

--- app/javascript/dashboard.js
+++ app/javascript/dashboard.js
@@ -1,23 +1,40 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { dashboard } from "controllers/dashboard"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import "controllers"
+import "@hotwired/turbo-rails"
+
 import * as bootstrap from "bootstrap";
 eagerLoadControllersFrom("dashboard", dashboard)
 
-const body = document.querySelector("body"),
-  sidebar = body.querySelector("nav"),
-  toggle = body.querySelector(".toggle"),
-  navLinkItems = sidebar.querySelectorAll('.nav-link');
+const sidebarNavWrapper = document.querySelector(".sidebar-nav-wrapper");
+const mainWrapper = document.querySelector(".main-wrapper");
+const menuToggleButton = document.querySelector("#menu-toggle");
+const menuToggleButtonIcon = document.querySelector("#menu-toggle i");
+const overlay = document.querySelector(".overlay");
 
-console.log(navLinkItems);
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
-});
+menuToggleButton.addEventListener("click", () => {
+  sidebarNavWrapper.classList.toggle("active");
+  overlay.classList.add("active");
+  mainWrapper.classList.toggle("active");
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 824) {
-    sidebar.classList.remove('close')
+  if (document.body.clientWidth > 1200) {
+    if (menuToggleButtonIcon.classList.contains("bi-caret-left")) {
+      menuToggleButtonIcon.classList.remove("bi-caret-left");
+      menuToggleButtonIcon.classList.add("bi-list");
+    } else {
+      menuToggleButtonIcon.classList.remove("bi-list");
+      menuToggleButtonIcon.classList.add("bi-caret-left");
+    }
   } else {
-    sidebar.classList.add('close')
+    if (menuToggleButtonIcon.classList.contains("bi-caret-left")) {
+      menuToggleButtonIcon.classList.remove("bi-caret-left");
+      menuToggleButtonIcon.classList.add("bi-list");
+    }
   }
+});
+overlay.addEventListener("click", () => {
+  sidebarNavWrapper.classList.remove("active");
+  overlay.classList.remove("active");
+  mainWrapper.classList.remove("active");
 });
