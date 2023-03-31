516db943c7e76640a9e7d9e83dbfdebbb3599d5f
Burak Ersoy <burakersoy@msn.com>
2023-03-31 04:14:05 +0300

    add before action hook to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!
   layout 'dashboard'
 
   def index; end
