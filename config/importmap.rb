b3b09f43cd8605c4a3d93d19bbf459ce83d24081
Burak Ersoy <burakersoy@msn.com>
2023-04-06 21:34:10 +0300

    comment out pinning bootstrap icons from cdn

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,15 +5,15 @@
 
 # Use npm packages from a JavaScript CDN by running ./bin/importmap
 
-pin 'app'
+pin 'application'
 pin 'devise'
 pin 'dashboard'
-pin '@hotwired/stimulus', to: 'stimulus.js'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js'
-pin 'bootstrap/icons', to: 'https://ga.jspm.io/npm:bootstrap-icons@1.9.1/font/bootstrap-icons.css'
-pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/esm/index.js'
+pin 'popper', to: 'popper.js', preload: true
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+# pin 'bootstrap/icons', to: 'https://ga.jspm.io/npm:bootstrap-icons@1.9.1/font/bootstrap-icons.css'
 pin 'stimulus', to: 'stimulus.js', preload: false # proxy for @hotwired/stimulus
 pin 'stimulus-password-visibility', to: 'https://ga.jspm.io/npm:stimulus-password-visibility@1.1.1/dist/stimulus-password-visibility.es.js'
